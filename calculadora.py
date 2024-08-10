f94a5e32d0aa023eab026219be2f3547e2e7e40b
ramon <ramonguerraospino@hotmail.es>
2024-08-09 22:14:05 -0500

    aprendido

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,15 @@
+print("horas extras")
+
+HoraTrabajo=  7659
+HoraExtraDominical = HoraTrabajo*2
+HoraExtraOrdinario = HoraTrabajo*float(1.25)
+
+Dominicales= input("total dominicales: ")
+ExtrasOrdinaria = input("total horas ordinarias: ")
+Tdominical= int(HoraExtraDominical*Dominicales)
+
+print(Tdominical)
 print("hola")
+print ("loli")
+print("hola hola")
+print("hola otra vez")
