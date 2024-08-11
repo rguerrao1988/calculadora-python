a0b461b90cb06719cb1496357ee9cd7959225f66
Ramon <155934578+rguerrao1988@users.noreply.github.com>
2024-08-10 19:05:21 -0500

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,19 @@
-print("horas extras")
-
-HoraTrabajo=  7659
-HD=HoraExtraDominical = HoraTrabajo*2
-HoraExtraOrdinario = HoraTrabajo*float(1.25)
-
-D=int(Dominicales= input("total dominicales: "))
-E=int(ExtrasOrdinaria = input("total horas ordinarias: "))
-Tdominical= (HD*D)
-
-print(Tdominical)
-print("hola")
-print ("loli")
-print("hola hola")
-print("hola otra vez")
+def calculadora(num_1, operacion, num_2):
+    
+    
+    if operacion == "+":
+        return num_1 + num_2
+    elif operacion == '-':
+        return num_1 - num_2
+    elif operacion == '*' or operacion == "x":
+        return num_1 * num_2
+    elif operacion == '/':
+        if num_2 != 0:
+            return num_1 / num_2
+        else:
+            return "Error: División por cero"
+    else:
+        return "Operación no válida"
+total = calculadora(int(input("primer numero: ")), str(input("operador? ")), int(input("segundo numero: ")),)
+# Ejemplos de uso:
+print(total)          # Output: 10
